4ae8abaecabdf892b83c2cd2d11bade419c716ab
ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>
2026-01-10 23:39:56 +0800

    Apply latest changes from upstream

--- servers/rendering/renderer_rd/effects/motion_blur.cpp
+++ servers/rendering/renderer_rd/effects/motion_blur.cpp
@@ -1,1 +1,62 @@
 ﻿#include "motion_blur.h"
+
+RendererRD::MotionBlur::MotionBlur() {
+	Vector<String> motion_blur_modes;
+
+	RD::SamplerState sampler;
+	sampler.mag_filter = RD::SAMPLER_FILTER_NEAREST;
+	sampler.min_filter = RD::SAMPLER_FILTER_NEAREST;
+	sampler.mip_filter = RD::SAMPLER_FILTER_NEAREST;
+	sampler.repeat_u = RD::SAMPLER_REPEAT_MODE_CLAMP_TO_EDGE;
+	sampler.repeat_v = RD::SAMPLER_REPEAT_MODE_CLAMP_TO_EDGE;
+	sampler.repeat_w = RD::SAMPLER_REPEAT_MODE_CLAMP_TO_EDGE;
+	motion_blur.nearest_sampler = RD::get_singleton()->sampler_create(sampler);
+
+	sampler.mag_filter = RD::SAMPLER_FILTER_LINEAR;
+	sampler.min_filter = RD::SAMPLER_FILTER_LINEAR;
+	sampler.mip_filter = RD::SAMPLER_FILTER_LINEAR;
+	motion_blur.linear_sampler = RD::get_singleton()->sampler_create(sampler);
+
+	motion_blur.preprocess_shader.initialize({ "" });
+	motion_blur.preprocess_shader_version = motion_blur.preprocess_shader.version_create();
+	motion_blur.pipelines[MOTION_BLUR_PREPROCESS].create_compute_pipeline(motion_blur.preprocess_shader.version_get_shader(motion_blur.preprocess_shader_version, 0));
+
+	motion_blur.tile_max_x_shader.initialize({ "" });
+	motion_blur.tile_max_x_shader_version = motion_blur.tile_max_x_shader.version_create();
+	motion_blur.pipelines[MOTION_BLUR_TILE_MAX_X].create_compute_pipeline(motion_blur.tile_max_x_shader.version_get_shader(motion_blur.tile_max_x_shader_version, 0));
+
+	motion_blur.tile_max_y_shader.initialize({ "" });
+	motion_blur.tile_max_y_shader_version = motion_blur.tile_max_y_shader.version_create();
+	motion_blur.pipelines[MOTION_BLUR_TILE_MAX_Y].create_compute_pipeline(motion_blur.tile_max_y_shader.version_get_shader(motion_blur.tile_max_y_shader_version, 0));
+
+	motion_blur.neighbor_max_shader.initialize({ "" });
+	motion_blur.neighbor_max_shader_version = motion_blur.neighbor_max_shader.version_create();
+	motion_blur.pipelines[MOTION_BLUR_NEIGHBOR_MAX].create_compute_pipeline(motion_blur.neighbor_max_shader.version_get_shader(motion_blur.neighbor_max_shader_version, 0));
+
+	motion_blur.blur_shader.initialize({ "" });
+	motion_blur.blur_shader_version = motion_blur.blur_shader.version_create();
+	motion_blur.pipelines[MOTION_BLUR_BLUR].create_compute_pipeline(motion_blur.blur_shader.version_get_shader(motion_blur.blur_shader_version, 0));
+}
+
+RendererRD::MotionBlur::~MotionBlur() {
+	for (int i = 0; i < MOTION_BLUR_MAX; i++) {
+		motion_blur.pipelines[i].free();
+	}
+
+	motion_blur.preprocess_shader.version_free(motion_blur.preprocess_shader_version);
+	motion_blur.tile_max_x_shader.version_free(motion_blur.tile_max_x_shader_version);
+	motion_blur.tile_max_y_shader.version_free(motion_blur.tile_max_y_shader_version);
+	motion_blur.neighbor_max_shader.version_free(motion_blur.neighbor_max_shader_version);
+	motion_blur.blur_shader.version_free(motion_blur.blur_shader_version);
+
+	RD::get_singleton()->free_rid(motion_blur.nearest_sampler);
+	RD::get_singleton()->free_rid(motion_blur.linear_sampler);
+}
+
+void RendererRD::MotionBlur::allocate_buffers(Ref<RenderSceneBuffersRD> p_render_buffers, MotionBlurBuffers &p_buffers, const Size2i &p_size, int p_tile_size) {
+
+}
+
+void RendererRD::MotionBlur::motion_blur_compute(const MotionBlurBuffers &p_buffers) {
+
+}
--- servers/rendering/renderer_rd/effects/motion_blur.h
+++ servers/rendering/renderer_rd/effects/motion_blur.h
@@ -3,20 +3,113 @@
 #include "servers/rendering/renderer_rd/pipeline_cache_rd.h"
 #include "servers/rendering/renderer_rd/pipeline_deferred_rd.h"
 #include "servers/rendering/renderer_rd/shaders/effects/motion_blur_blur.glsl.gen.h"
+#include "servers/rendering/renderer_rd/shaders/effects/motion_blur_neighbor_max.glsl.gen.h"
+#include "servers/rendering/renderer_rd/shaders/effects/motion_blur_preprocess.glsl.gen.h"
+#include "servers/rendering/renderer_rd/shaders/effects/motion_blur_tile_max_x.glsl.gen.h"
+#include "servers/rendering/renderer_rd/shaders/effects/motion_blur_tile_max_y.glsl.gen.h"
+#include "servers/rendering/renderer_rd/storage_rd/render_scene_buffers_rd.h"
+
+#define RB_SCOPE_MOTION_BLUR SNAME("motion_blur")
+
+#define RB_TEX_TILE_MAX_X SNAME("tile_max_x")
+#define RB_TEX_TILE_MAX_Y SNAME("tile_max_y")
+#define RB_TEX_NEIGHBOR_MAX SNAME("neighbor_max")
+#define RB_TEX_BLUR_OUTPUT SNAME("blur_output")
+
+namespace RendererRD {
 
 class MotionBlur {
-public:
+private:
 	enum MotionBlurMode {
 		MOTION_BLUR_PREPROCESS,
-		MOTION_BLUR_STANDARD,
-		MOTION_BLUR_BOKEH,
+		MOTION_BLUR_TILE_MAX_X,
+		MOTION_BLUR_TILE_MAX_Y,
+		MOTION_BLUR_NEIGHBOR_MAX,
+		MOTION_BLUR_BLUR,
 		MOTION_BLUR_MAX,
 	};
 
-	struct {
-		MotionBlurBlurShaderRD blur_shader;
-		RID shader_version;
-		PipelineDeferredRD compute_pipelines[MOTION_BLUR_MAX];
-	} shaders;
+	struct MotionBlurPreprocessPushConstant {
+		float rotation_velocity_multiplier;
+		float movement_velocity_multiplier;
+		float object_velocity_multiplier;
+		float rotation_velocity_lower_threshold;
 
+		float movement_velocity_lower_threshold;
+		float object_velocity_lower_threshold;
+		float rotation_velocity_upper_threshold;
+		float movement_velocity_upper_threshold;
+
+		float object_velocity_upper_threshold;
+		float is_fsr2;
+		float motion_blur_intensity;
+		float pad;
+	};
+
+	struct MotionBlurTileMaxPushConstant {
+		int32_t tile_size;
+		int32_t pad[3];
+	};
+
+	struct  MotionBlurBlurPushConstant {
+		float minimum_user_threshold;
+		float importance_bias;
+		float maximum_jitter_value;
+		float motion_blur_intensity;
+
+		int32_t tile_size;
+		int32_t sample_count;
+		int32_t frame;
+		int32_t pad;
+	};
+
+	struct {
+		MotionBlurPreprocessPushConstant preprocess_push_constant;
+		MotionBlurTileMaxPushConstant tile_max_push_constant;
+		MotionBlurBlurPushConstant blur_push_constant;
+
+		MotionBlurPreprocessShaderRD preprocess_shader;
+		RID preprocess_shader_version;
+
+		MotionBlurTileMaxXShaderRD tile_max_x_shader;
+		RID tile_max_x_shader_version;
+
+		MotionBlurTileMaxYShaderRD tile_max_y_shader;
+		RID tile_max_y_shader_version;
+
+		MotionBlurNeighborMaxShaderRD neighbor_max_shader;
+		RID neighbor_max_shader_version;
+
+		MotionBlurBlurShaderRD blur_shader;
+		RID blur_shader_version;
+
+		PipelineDeferredRD pipelines[MOTION_BLUR_MAX];
+		RID linear_sampler;
+		RID nearest_sampler;
+	} motion_blur;
+public:
+	struct MotionBlurBuffers {
+		int tile_size;
+
+		Size2i base_texture_size;
+		// (base.x / tile_size, base.y)
+		Size2i tile_x_texture_size;
+		// (base.x / tile_size, base.y / tile_size)
+		Size2i tile_y_texture_size;
+
+		// textures
+		RID base_texture;
+		RID velocity_texture;
+		RID tile_max_x_texture;
+		RID tile_max_y_texture;
+		RID neighbor_max_texture;
+		RID output_texture;
+	};
+
+	MotionBlur();
+	~MotionBlur();
+
+	void allocate_buffers(Ref<RenderSceneBuffersRD> p_render_buffers, MotionBlurBuffers& p_buffers, const Size2i &p_size, int p_tile_size);
+	void motion_blur_compute(const MotionBlurBuffers &p_buffers);
 };
+}
